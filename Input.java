7f83727169905e1ff9b65fa0d7d5efd542f2d268
parthmagar446 <parthmagar446@example.com>
2026-07-10 23:58:41 +0530

    learn to input

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input{
+    public static void main (String[] args)
+    {
+        System.out.println("enter the roll no:");
+        Scanner input=new Scanner(System.in);
+        int rollno=input.nextInt();
+        System.out.println("roll no="+rollno);
+    }
+    
+}
