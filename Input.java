5ddb4f44ebd77facc8acdae76353216ce135700d
parthmagar446 <parthmagar446@example.com>
2026-07-11 00:10:49 +0530

    input a string

--- Input.java
+++ Input.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 public class Input{
     public static void main (String[] args)
     {
-        System.out.println("enter the roll no:");
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=input.nextLine();
+        System.out.println("Name="+name);
+        System.out.println("enter the roll no:");
         int rollno=input.nextInt();
         System.out.println("roll no="+rollno);
     }
