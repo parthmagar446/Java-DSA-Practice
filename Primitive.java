02cf4c55b412813977b5ec14932337280e7bcb86
parthmagar446 <parthmagar446@example.com>
2026-07-10 23:35:05 +0530

    types of primitives added

---
+++ Primitive.java
@@ -0,0 +1,31 @@
+public class Primitive {
+    public static void main(String[] args)
+    {
+        //number
+        int a=10;
+
+        //character
+        char b='p';
+
+        //decimal number
+        float c=9.5f;
+
+        //true or false
+        boolean d=true;
+
+        //big decimal number
+        double e=6589674.543;
+
+        //big integer number
+        long f=548135l;
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+        System.out.println("e="+e);
+        System.out.println("f="+f);
+
+    }
+    
+}
