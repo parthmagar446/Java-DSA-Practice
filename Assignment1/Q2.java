6600c60400556b91f8de14edc0c03d720b3adef6
parthmagar446 <parthmagar446@example.com>
2026-07-11 14:43:03 +0530

    Added Q2 (greeting message) program in Assignment1

---
+++ Assignment1/Q2.java
@@ -0,0 +1,12 @@
+package Assignment1;
+import java.util.Scanner;
+public class Q2 {
+
+    public static void main(String[] args)
+    {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=input.nextLine();
+        System.out.println("welcome "+name);
+    }
+}
