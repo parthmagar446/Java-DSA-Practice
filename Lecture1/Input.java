2d432d244a3e808055382eba893051834e587f16
parthmagar446 <parthmagar446@example.com>
2026-07-11 13:58:08 +0530

    Organized previous practice programs into Lecture1 folder

---
+++ Lecture1/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Input{
+    public static void main (String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=input.nextLine();
+        System.out.println("Name="+name);
+        System.out.println("enter the roll no:");
+        int rollno=input.nextInt();
+        System.out.println("roll no="+rollno);
+    }
+    
+}
