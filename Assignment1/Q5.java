501fc13b4b01409daa555cf7334492b56af7fa3f
parthmagar446 <parthmagar446@example.com>
2026-07-13 22:40:20 +0530

    Added Q5(greatest integer) problem in Assignment1

---
+++ Assignment1/Q5.java
@@ -0,0 +1,21 @@
+package Assignment1;
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter num1:");
+            int num1 = input.nextInt();
+            System.out.println("Enter num2:");
+            int num2 = input.nextInt();
+
+            if (num1 > num2) {
+                System.out.println(num1 + " is greater");
+            } else if (num2 > num1) {
+                System.out.println(num2 + " is greater");
+            } else {
+                System.out.println("Both the numbers are equal");
+            }
+        }
+    }
+}
