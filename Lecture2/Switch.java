633105ddbe0a5b3f0cacad798141a4e8d1f4f863
parthmagar446 <parthmagar446@gmail.com>
2026-07-15 00:17:37 +0530

    Added Switch case in Lecture 2

---
+++ Lecture2/Switch.java
@@ -0,0 +1,29 @@
+package Lecture2;
+import java.util.Scanner;
+public class Switch {
+    public static void main(String [] args)
+    {
+        Scanner in= new Scanner(System.in);
+        System.out.println("enter the number of the day in a week(1-7)");
+        int Day=in.nextInt();
+        switch(Day)
+        {
+            case 1:System.out.println("monday");
+                break;
+            case 2:System.out.println("tueday");
+                break;
+             case 3:System.out.println("wednesday");
+                break;
+            case 4:System.out.println("thursday");
+                break;
+            case 5:System.out.println("friday");
+                break;
+            case 6:System.out.println("saturday");
+                break;
+            case 7:System.out.println("sunday");
+                break;
+            default:System.out.println("invalid input");
+
+        }
+    }    
+}
