d05a507c77b5642c673552776d1c4967b5fba747
parthmagar446 <parthmagar446@gmail.com>
2026-07-14 15:36:47 +0530

    Added Q2(table) problem in Assignment1

---
+++ Assignment1/Q2.java
@@ -0,0 +1,16 @@
+package Assignment1;
+import java.util.Scanner;
+public class Q2 {
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=in.nextInt();
+        for(int i=0;i<=10;i++)
+        {
+            int pro=num*i;
+            System.out.printf("%d * %d = %d\n",num,i,pro);
+        }
+        in.close();
+    }    
+}
