61ea5c9c9e91970d0b9bd7349f21ac2da555029b
parthmagar446 <parthmagar446@gmail.com>
2026-07-14 15:01:59 +0530

    Assignment 1 added

---
+++ Assignment1/Q1.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the year:");
+        int year = in.nextInt();
+
+        if (year % 4 == 0) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
